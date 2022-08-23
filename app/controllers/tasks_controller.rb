81f0f55411021c1e7c3d7744cdecd96207170045
Nicolas BEURIOT <nbeuriot@gmail.com>
2022-08-23 15:46:33 +0200

    Rails-CRUD-Exo 2 Part 7- Done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
@@ -18,7 +18,26 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to index_path, status: :see_other
+  end
+
+  private
+
   def set_task
     @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # CRUD
+  # READ ALL
   get 'tasks', to: 'tasks#index', as: :index
+  # CREATE
   get 'tasks/new', to: 'tasks#new', as: :new_tasks
   post 'tasks', to: 'tasks#create'
-
+  # READ ONE
   get "tasks/:id", to: "tasks#show", as: :task
+  # UPDATE
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  # DELETE
+  delete "tasks/:id", to: "tasks#destroy"
 end
